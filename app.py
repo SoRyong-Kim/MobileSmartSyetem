e9bc00af9e7013725637c662633d15390b1ab845
SrTime <72728663+SrTime@users.noreply.github.com>
2023-12-01 15:52:06 +0900

    이건 led, connect가 안되는 코드들

--- app.py
+++ app.py
@@ -4,17 +4,33 @@ from flask import Flask, render_template, request
 import paho.mqtt.client as mqtt
 import os
 import threading
+import sound_detector
 from time import sleep  # time 모듈에서 sleep 함수 import
 
 app = Flask(__name__)
 client = mqtt.Client()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mqtt', methods=['GET', 'POST'])
+def mqtt():
+    # MQTT 브로커에 연결합니다.
+    client.connect("localhost", 1883, 60)
+
+    # 'test' 토픽에 메시지를 발행합니다.
+    client.publish("test", "Hello MQTT")
+
+    return "MQTT route"
 
 def on_connect(client, userdata, flag, rc):
     client.subscribe("led", qos=0)  # "led" 토픽으로 구독 신청
 
 def on_message(client, userdata, msg):
     on_off = int(msg.payload)  # on_off는 0 또는 1의 정수
-    circuit.controlLED(on_off)  # LED를 켜거나 끔
+    sound_detector.controlLED(on_off)  # LED를 켜거나 끔
+
 
 client.on_connect = on_connect
 client.on_message = on_message
@@ -23,9 +39,12 @@ def play_alarm(total_seconds):
     sleep(total_seconds)
     os.system('cvlc /home/pi/static/alarm.mp3 --play-and-exit')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def start_mqtt():
+    client.connect("localhost", 1883, 60)
+    client.loop_start()
+
+def stop_mqtt():
+    client.loop_stop()
 
 @app.route('/set_alarm', methods=['POST'])
 def set_alarm():
@@ -45,5 +64,19 @@ def stop():
     os.system('pkill vlc')
     return 'Alarm stopped.'
 
+@app.route('/start_sound_measurement', methods=['GET'])
+def start_sound_measurement():
+    start_mqtt()
+    return 'Sound measurement started.'
+
+@app.route('/stop_sound_measurement', methods=['GET'])
+def stop_sound_measurement():
+    stop_mqtt()
+    return str(sound_data)
+
+@app.route('/get_sound_data', methods=['GET'])
+def get_sound_data():
+    return str(sound_data)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=9001)
--- mqtt_alarm.py
+++ mqtt_alarm.py
@@ -1,7 +1,7 @@
-# mqtt_alarm.py
 import paho.mqtt.client as mqtt
 import time
-import sound_detector  # 이 부분을 수정
+import os
+import sound_detector  # sound_detector 모듈 import
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -13,6 +13,8 @@ def on_message(client, userdata, msg):
     time.sleep(total_seconds)
     os.system('cvlc /home/pi/static/alarm.mp3 --play-and-exit')
 
+    
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -22,8 +24,8 @@ client.loop_start()
 
 while True:
     soundlevel = sound_detector.measure_soundlevel()
-    client.publish("sound", soundlevel, qos=0)  # “ultrasonic” 토픽으로 거리 전송
+    client.publish("sound", soundlevel, qos=0)  # “sound” 토픽으로 사운드 센서 값 전송
     time.sleep(1)
 
-client.loop_stop()
-client.disconnect()
+client.loop_stop()  # 루프를 중지하지 않음 (계속해서 메시지 수신을 위해)
+client.disconnect()  # 연결 해제하지 않음 (계속해서 메시지 수신을 위해)
