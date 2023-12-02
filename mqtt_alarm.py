8cad090016c5c75710e210e950c3465db4829c7f
SrTime <72728663+SrTime@users.noreply.github.com>
2023-12-02 17:54:41 +0900

    - 알람 잘 나옴. - led안됨 - 연결 잘 됨.

--- mqtt_alarm.py
+++ mqtt_alarm.py
@@ -1,26 +1,30 @@
+# mqtt_alarm.py
 import paho.mqtt.client as mqtt
 import time
 import os
-import sound_detector  # sound_detector 모듈 import
+import sound_detector
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("led", qos=0)  # "led" 토픽으로 구독 신청
+    print("Connected with result code "+str(rc))
+    client.subscribe("alarm/topic")
 
 def on_message(client, userdata, msg):
-    on_off = int(msg.payload)  # on_off는 0 또는 1의 정수
-    sound_detector.controlLED(on_off)  # LED를 켜거나 끔 ********************
+    total_seconds = int(msg.payload.decode())
+    print(f"알람이 {total_seconds} 초 뒤에 알람이 울립니다")
+    time.sleep(total_seconds)
+    os.system('cvlc /home/pi/static/alarm.mp3 --play-and-exit')
 
-ip = "localhost"
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(ip, 1883, 60)
+
+client.connect("localhost", 1883, 60)
 client.loop_start()
 
 while True:
-    soundlevel = sound_detector.measure_soundlevel()
-    client.publish("sound", soundlevel, qos=0)  # “sound” 토픽으로 사운드 센서 값 전송
+    sound = sound_detector.measure_sound()  # 사운드 센서로부터 사운드값 읽기
+    client.publish("sound", sound, qos=0)  # “ultrasonic” 토픽으로 사운드센서 값 전송
     time.sleep(1)
 
-client.loop_stop()  # 루프를 중지하지 않음 (계속해서 메시지 수신을 위해)
-client.disconnect()  # 연결 해제하지 않음 (계속해서 메시지 수신을 위해)
+client.loop_stop()
+client.disconnect()
--- sound_detector.py
+++ sound_detector.py
@@ -1,39 +1,16 @@
-# sound_detector.py
-import os
 import RPi.GPIO as GPIO
 import time
 import paho.mqtt.client as mqtt
-from time import sleep  # time 모듈에서 sleep 함수 import
 
-ip = "localhost" 
-client = mqtt.Client()
-client.connect(ip, 1883, 60)
-
-# LED를 켜고 끄는 함수
-def controlLED(on_off): 
-    # led 번호의 핀에 on_off(0/1) 값 출력하는 함수
-    GPIO.output(led, on_off)
-
-def measure_soundlevel():
-    try:
-        while True:
-            soundlevel = GPIO.input(soundpin)
-            if soundlevel == 1:
-                client.publish("sound", soundlevel, qos=0)
-            time.sleep(1) # 몇 초 단위로 확인할 지
-
-    except KeyboardInterrupt:
-        print("테스트 종료")
-        GPIO.cleanup()
-        
-# LED를 다루기 위한 전역 변수 선언 및 초기화
-led = 12  # GPIO6
-GPIO.setup(led, GPIO.OUT)  # GPIO6 핀을 출력으로 지정
-
-soundpin = 19
 GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.cleanup()
-
-GPIO.setup(led, GPIO.OUT)  # GPIO6 핀을 출력으로 지정
+soundpin = 19
 GPIO.setup(soundpin, GPIO.IN)
+
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
+
+while True:
+    soundlevel = GPIO.input(soundpin)
+    if soundlevel == 1:
+        client.publish("sound", "sound detected")
+    time.sleep(1)
